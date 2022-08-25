454733551a84a3a89112bdfe2be16791a44977db
Tzlil <tzlils@protonmail.com>
2022-08-25 07:29:04 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         AWS Username Detector
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Display logged in AWS username in card
+// @author       Tzlil
+// @match        https://hibbard.eu/tampermonkey-tutorial/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=hibbard.eu
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
